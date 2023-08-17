49e91648eb47c7badc48a0770265ff0b471934e9
aniket <aniket.umbc@gmail.com>
2023-08-17 20:01:43 +0530

    Explicit assertion

--- cypress/e2e/Assertion.cy.js
+++ cypress/e2e/Assertion.cy.js
@@ -12,38 +12,32 @@ describe('Assertion demo', () => {
       )
       .should('contain', 'orangehrmlive')
       .should('not.contain', 'green');
-  });
-
-  it('Should have correct title', () => {
-    cy.visit(
-      'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    );
-    //should and
     cy.title().should('include', 'OrangeHRM').and('eq', 'OrangeHRM');
-  });
-
-  it('Should have correct logo', () => {
-    cy.visit(
-      'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    );
-    //should and
     cy.get('.orangehrm-login-branding > img').should('be.visible').and('exist');
-  });
-
-  it('Should captures correct link a tag', () => {
-    cy.visit(
-      'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    );
-    //should and
     cy.xpath('//a').should('have.length', '5'); // Number of link
-  });
 
-  it('Should correct username input type', () => {
-    cy.visit(
-      'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    );
-    //should and
     cy.get("input[placeholder='Username']").type('Admin'); // value to inputbox
     cy.get("input[placeholder='Username']").should('have.value', 'Admin');
+
+    cy.get("input[placeholder='Username']").type('Mike'); // value to inputbox
+    cy.get("input[placeholder='Username']").should('not.have.value', 'Admin');
+  });
+
+  it('Explict Assertion', () => {
+    cy.visit(
+      'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    );
+
+    cy.get("input[placeholder='Username']").type('Admin');
+    cy.get("input[placeholder='Password']").type('admin123');
+
+    cy.get("button[type='submit']").click();
+
+    let expectedName = 'sivabalaji A';
+    cy.get('.oxd-userdropdown-name').then((value) => {
+      let actualName = value.text();
+
+      expect(actualName).to.equal(expectedName);
+    });
   });
 });
