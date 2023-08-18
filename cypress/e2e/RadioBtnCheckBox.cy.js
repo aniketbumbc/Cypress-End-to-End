d8529074935b02e58a633fc93e4939b6c75b03ea
aniket <aniket.umbc@gmail.com>
2023-08-18 19:02:05 +0530

    Drop down testcases

--- cypress/e2e/RadioBtnCheckBox.cy.js
+++ cypress/e2e/RadioBtnCheckBox.cy.js
@@ -20,4 +20,45 @@ describe('Check UI Element on websites ', () => {
     cy.get('#html').should('not.be.checked');
     cy.get('#css').should('not.be.checked');
   });
+
+  it('Checking checkbox', () => {
+    cy.visit('https://7rdspt.csb.app/');
+
+    //visibility of checkbox
+    cy.get('#vehicle1').should('be.visible');
+    cy.get('#vehicle2').should('be.visible');
+    cy.get('#vehicle3').should('be.visible');
+    cy.get('#vehicle33').should('not.exist');
+
+    cy.get('#vehicle2').check().should('be.checked');
+    cy.get('#vehicle1').should('not.be.checked');
+    cy.get('#vehicle3').should('not.be.checked');
+
+    cy.get('#vehicle2').uncheck().should('not.be.checked');
+
+    //selecting all checkbox
+
+    cy.get("input.test-checkbox[type$='checkbox']")
+      .check()
+      .should('be.checked');
+
+    cy.get("input.test-checkbox[type$='checkbox']")
+      .uncheck()
+      .should('not.be.checked');
+
+    cy.get("input.test-checkbox[type$='checkbox']")
+      .first()
+      .check()
+      .should('be.checked');
+
+    cy.get("input.test-checkbox[type$='checkbox']")
+      .last()
+      .check()
+      .should('be.checked');
+
+    cy.get("input.test-checkbox[type$='checkbox']")
+      .first()
+      .uncheck()
+      .should('not.be.checked');
+  });
 });
---
+++ cypress/e2e/dropDown.cy.js
@@ -0,0 +1,45 @@
+describe('handle dropdown', () => {
+  it.skip('Dropdown selector', () => {
+    cy.visit('https://7rdspt.csb.app/');
+
+    cy.get('#cars').select('Audi').should('have.value', 'audi');
+  });
+
+  it.skip('Dropdown selector', () => {
+    cy.visit('https://www.dummyticket.com/dummy-ticket-for-visa-application/');
+
+    cy.get('#select2-billing_country-container').click();
+
+    cy.get("input[role='combobox']").type('Italy').type('{enter}');
+
+    cy.get('#select2-billing_country-container').should('have.text', 'Italy');
+
+    cy.get('#select2-billing_country-container').click();
+    cy.get("input[role='combobox']").type('Japan').type('{enter}');
+    cy.get('#select2-billing_country-container').should('have.text', 'Japan');
+  });
+
+  it.skip('Dropdown auto suggest', () => {
+    cy.visit('https://www.wikipedia.org/');
+
+    cy.get('#searchInput').type('Mumbai');
+
+    cy.get('.suggestion-title').contains('Mumbai Metro').click();
+    cy.get('.mw-page-title-main').contains('Mumbai Metro');
+  });
+
+  it('Dropdown auto suggest', () => {
+    cy.visit('https://www.google.com/');
+
+    cy.get('#APjFqb').type('Mumbai');
+    cy.get('div.wM6W7d>span').should('have.length', 12);
+
+    cy.get('div.wM6W7d>span').each(($el, index, $list) => {
+      if ($el.text() == 'Mumbai') {
+        cy.wrap($el).click();
+      }
+    });
+
+    cy.get('#APjFqb').should('have.value', 'Mumbai');
+  });
+});
