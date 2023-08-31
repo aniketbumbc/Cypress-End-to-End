51c12e46d8fcc129a4237d3ab7d8e3c0dead7022
aniket <aniket.umbc@gmail.com>
2023-08-31 06:38:45 +0530

    reports and video of test file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
+const { defineConfig } = require('cypress');
+
 module.exports = {
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      screenshotFailture = true;
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 };
